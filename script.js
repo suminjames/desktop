d9f933e5c2667185894bcefe5c4ac37de7f5c8e3
Sumin James <suminjames6@gmail.com>
2019-06-04 14:53:50 +0545

    desktop and menu are functional

--- script.js
+++ script.js
@@ -4,21 +4,54 @@ var $ = function(query) {
 
 var desktop = {
     background: "#123333",
-    height: '100%',
+    height: '90%',
     width: '100%',
-    backgroundPosition: "center",
-    backgroundSize: "cover",
+    backgroundSize: "auto",
     render: function() {
-        var desktop = document.querySelector('.desktop');
+        var desktop = $('.desktop');
         desktop.style.height = this.height;
         desktop.style.width = this.width;
-        desktop.style.backgroundSize = this.backgroundSize;
         desktop.style.background = this.background;
-        desktop.style.backgroundPosition = this.backgroundPosition;
+        desktop.style.backgroundSize = this.backgroundSize;
+        console.log(desktop.style.backgroundSize);
+    },
+
+    save: function() {
+        var model = {
+            background: this.background,
+            height: this.height,
+            width: this.width,
+            backgroundSize: this.backgroundSize
+        }
+
+        localStorage.setItem('desktop', JSON.stringify(model))
+    },
+
+    // contextmenu: function(event) {
+    //     var menu = $('.menu');
+    //     menu.classList.toggle("hidden");
+    //     menu.style.top = event.clientY - 15 + "px";
+    //     menu.style.left = event.clientX + "px";
+    // }
+}
+
+bootloader = function() {
+    storeddata = localStorage.getItem('desktop');
+    actualData = JSON.parse(storeddata);
+    if (!!actualData) {
+        desktop.background = actualData.background;
+        desktop.height = actualData.height;
+        desktop.width = actualData.width;
+        desktop.backgroundSize = actualData.backgroundSize;
     }
 }
-desktop.background = 'yellow'
+debugger
+bootloader();
 desktop.render();
+
+setInterval(() => {
+    desktop.save()
+}, 2000);
 
 var menu = {
     changeBg: function() {
@@ -29,9 +62,7 @@ var menu = {
             var reader = new FileReader();
 
             reader.addEventListener("load", function() {
-
                 desktop.background = "url(" + reader.result + ")";
-
                 desktop.render();
             }, false);
 
@@ -39,9 +70,31 @@ var menu = {
                 reader.readAsDataURL(fileObject.files[0]);
             }
         });
+    },
 
+    changeBgSize: function() {
+        desktop.backgroundSize = "contain";
+        desktop.render();
+    },
+
+    changeBgSizeCover: function() {
+        desktop.backgroundSize = "cover";
+        desktop.render();
     }
 }
 
 
-$('#changeBg').addEventListener('click', menu.changeBg)
+$('#changeBg').addEventListener('click', menu.changeBg);
+$('#bgSizeContain').addEventListener('click', menu.changeBgSize);
+$('#bgSizeCover').addEventListener('click', menu.changeBgSizeCover);
+
+var menu = $('.menu');
+$('.desktop').addEventListener('contextmenu', function() {
+    menu.classList.toggle("hidden");
+    menu.style.top = event.clientY - 15 + "px";
+    menu.style.left = event.clientX + "px";
+})
+
+$('.desktop').addEventListener('click', function() {
+    menu.classList.add("hidden");
+})
