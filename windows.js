35a37840842b350bdfaeecdaad3b3deb25c2fcc7
Sumin James <suminjames6@gmail.com>
2019-06-04 16:10:44 +0545

    dragging windows are functional

--- windows.js
+++ windows.js
@@ -1,3 +1,38 @@
+var elemnt;
+
+function dragMouseDown(e) {
+    e = e || window.event;
+    elemnt = this;
+    e.preventDefault();
+    //mouse position at startup
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call function when mouse moves
+    document.onmousemove = elementDrag;
+}
+
+function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate new cursor position
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element to the new calculated
+    // e.target.style.left = 0;
+    // e.target.style.top = 0;
+    console.log(elemnt)
+    elemnt.style.top = elemnt.offsetTop - pos2 + "px";
+    elemnt.style.left = elemnt.offsetLeft - pos1 + "px";
+}
+
+function closeDragElement() {
+    // stop moving when the mouse button is released
+    document.onmouseup = null;
+    document.onmousemove = null;
+}
 var ourWindow = function() {
     this.model = {
         height: '500px',
@@ -35,6 +70,7 @@ var ourWindow = function() {
         cloneWindow.id = "random";
         cloneWindow.classList.remove('hidden');
         var desktop = $('.desktop');
+        cloneWindow.addEventListener('mousedown', dragMouseDown);
         desktop.append(cloneWindow);
     }
 
